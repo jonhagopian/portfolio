a5535f2ca6abd15a9ecf30c56668ee426e796239
Jon Hagopian <jon.hagopian@gmail.com>
2017-08-12 00:12:39 -0400

    update eventListener

---
+++ resources/js/image_slider.js
@@ -0,0 +1,39 @@
+function imageSlider() {
+  var imageSlider = document.getElementById('image_slider');
+  var firstSlide = imageSlider.getElementsByTagName('div')[0];
+  var slideWidth = firstSlide.offsetWidth;
+  var slideMarWidth = parseInt(getComputedStyle(firstSlide).marginRight);
+  var fullSlideWidth = slideWidth + slideMarWidth;
+  var slideMarginRight = parseInt(getComputedStyle(firstSlide).marginRight);
+  var imageSlidesArray = document.getElementsByClassName('image_slide');
+  var imageWidth = firstSlide.getElementsByTagName('img')[0].offsetWidth;
+  function slideAnimation() {
+    var imageSliderWidth = imageSlider.offsetWidth;
+    var scrollPos = imageSlider.scrollLeft;
+    if (fullSlideWidth * (imageSlidesArray.length - 1) <= imageSliderWidth ) {
+      image_slider.setAttribute('class','justified');
+    } else {
+      for (var i = 0; i < imageSlidesArray.length; i++) {
+        var slide = imageSlidesArray[i];
+        //current slides center, distance from SCROLLER containers left side
+        var slidePos = Math.round(imageSlidesArray[i].offsetLeft + (fullSlideWidth / 2) );
+        //current slides center position IN CONTAINER
+        var slidePosIn = scrollPos - slidePos;
+        //convert to percentage, this is value to move images within slide right
+        var slidePosPct = Math.round((slidePosIn / imageSliderWidth) * 100);
+        //move image to center using 1/2 image width which is 50%
+        slidePosPct = slidePosPct + 50;
+        //finally... SLOW down image movement by cutting down slide pos percentage
+        slidePosPct = slidePosPct * 0.20;
+        moveMe = slidePosPct;
+        imageSlidesArray[i].getElementsByTagName('img')[0].style.transform = 'translateX(' + moveMe + '%)';
+      }
+    }
+  }
+  imageSlider.addEventListener('scroll', slideAnimation);
+}; //EOF
+//initialize image slider
+window.addEventListener('load', imageSlider);
+//initialize image slider if window resized or orientation change
+window.addEventListener('onresize', imageSlider);
+window.addEventListener('orientationchange', imageSlider);
