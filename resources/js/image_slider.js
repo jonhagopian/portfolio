681022bfa3f05dcc01c940c08589ae11d405e89d
Jon Hagopian <jon.hagopian@gmail.com>
2017-08-12 14:06:08 -0400

    cleaned up

--- resources/js/image_slider.js
+++ resources/js/image_slider.js
@@ -2,11 +2,8 @@ function imageSlider() {
   var imageSlider = document.getElementById('image_slider');
   var firstSlide = imageSlider.getElementsByTagName('div')[0];
   var slideWidth = firstSlide.offsetWidth;
-  var slideMarWidth = parseInt(getComputedStyle(firstSlide).marginRight);
-  var fullSlideWidth = slideWidth + slideMarWidth;
-  var slideMarginRight = parseInt(getComputedStyle(firstSlide).marginRight);
+  var fullSlideWidth = slideWidth + parseInt(getComputedStyle(firstSlide).marginRight);
   var imageSlidesArray = document.getElementsByClassName('image_slide');
-  var imageWidth = firstSlide.getElementsByTagName('img')[0].offsetWidth;
   function slideAnimation() {
     var imageSliderWidth = imageSlider.offsetWidth;
     var scrollPos = imageSlider.scrollLeft;
@@ -19,14 +16,13 @@ function imageSlider() {
         var slidePos = Math.round(imageSlidesArray[i].offsetLeft + (fullSlideWidth / 2) );
         //current slides center position IN CONTAINER
         var slidePosIn = scrollPos - slidePos;
-        //convert to percentage, this is value to move images within slide right
+        //convert to percentage, this is the value to move images within slide right
         var slidePosPct = Math.round((slidePosIn / imageSliderWidth) * 100);
         //move image to center using 1/2 image width which is 50%
         slidePosPct = slidePosPct + 50;
         //finally... SLOW down image movement by cutting down slide pos percentage
         slidePosPct = slidePosPct * 0.20;
-        moveMe = slidePosPct;
-        imageSlidesArray[i].getElementsByTagName('img')[0].style.transform = 'translateX(' + moveMe + '%)';
+        imageSlidesArray[i].getElementsByTagName('img')[0].style.transform = 'translateX(' + slidePosPct + '%)';
       }
     }
   }
