683b576c663fcbbb6d163f7dcc00e88bc9cd3dfa
Jon Hagopian <jon.hagopian@gmail.com>
2017-08-26 11:39:59 -0400

    updated slider

--- resources/js/image_slider.js
+++ resources/js/image_slider.js
@@ -1,22 +1,21 @@
-// Image Slider
-function sAnimate(box, boxW, sW, sMR, sOffsetArr, sImgArr) {
-  var scrPos = box.scrollLeft;
-  for (var i = 0; i < sOffsetArr.length; i++) {
-    var sPos = sOffsetArr[i] + ((sW+sMR) / 2);
-    var sPosVis = scrPos - sPos;
-    var sPosPct = (sPosVis / boxW) * 100;
-    sPosPct = sPosPct + 50;
-    sPosPct = (sPosPct * 0.20).toFixed(4); // slow image movement by reducing this %, round to two decimal
-    sImgArr[i].style.transform = "translateX(" + sPosPct + "%)";
-  }
-} //EOF
-
+// Image Gallery Slider
 function imageSlider(firstRun) {
   if (firstRun === true) {
-    //re-init slider
+    // Re-init slider
     window.addEventListener("resize", imageSlider);
     window.addEventListener("orientationchange", imageSlider);
   }
+  function sAnimate(box, boxW, sW, sMR, sOffsetArr, sImgArr) {
+    var scrPos = box.scrollLeft;
+    for (var i = 0; i < sOffsetArr.length; i++) {
+      var sPos = sOffsetArr[i] + ((sW+sMR) / 2);
+      var sPosVis = scrPos - sPos;
+      var sPosPct = (sPosVis / boxW) * 100;
+      sPosPct = sPosPct + 50;
+      sPosPct = (sPosPct * 0.20).toFixed(2); // slow image movement by reducing this %
+      sImgArr[i].style.transform = "translateX(" + sPosPct + "%)";
+    }
+  } //EOF
   var allSliders = document.querySelectorAll(".image_slider");
   // For each individual slider 'section' element
   for (var j = 0; j < allSliders.length; j++) {
@@ -27,7 +26,7 @@ function imageSlider(firstRun) {
     let sMR = parseFloat(getComputedStyle(s0).marginRight);
     let sF = sW + sMR;
     let sArr = box.querySelectorAll(".image_slide");
-    // If screen is too wide, there is no need to run the slider
+    // If screen is too wide, no need to run slider
     if (sF * (sArr.length - 1) <= boxW) {
       box.setAttribute("class","image_slider justified");
       box.scrollLeft = 0;
@@ -48,8 +47,8 @@ function imageSlider(firstRun) {
   }
 } //EOF
 
-//init slider
+// Init Slider
 window.addEventListener("load", function() {
   imageSlider(true);
 });
-// End Image Slider
+// End Image Gallery Slider
