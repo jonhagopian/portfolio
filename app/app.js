4902dca85a1fc6f1dc1b8f8e2afc086cbf9c5dc3
Jon Hagopian <jon.hagopian@gmail.com>
2017-10-05 22:47:32 -0400

    nodejs integration v0

---
+++ app/app.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var reload = require("reload"); // auto browser reload module, use for local testing
+
+var app = express();
+
+app.set("port", process.env.PORT || 3000)
+app.set("view engine", "ejs");
+app.set("views", "app/views");
+
+// Global Variables
+app.locals.siteTitle = "Jon Hagopian";
+//app.locals.defaultResources = "app/resources";
+
+// IS the where you would set the AWS CDN URL?
+app.use(express.static('app/resources'));
+// Set Routes
+app.use(require('./routes/route-index'));
+
+var server = app.listen(app.get('port'), function() {
+  console.log('Listening on port ' + app.get('port'));
+});
+
+// start reload for testing local
+reload(app);
---
+++ app/routes/route-index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(request, response) {
+//  var data = request.app.get('appData');
+//  data.whatData.forEach(function(item) {
+//    var = var.concat(who.what);
+//  });
+
+  response.render('index', {
+    pageTitle: 'Home',
+    pageID: 'home'
+  });
+
+});
+
+module.exports = router;
