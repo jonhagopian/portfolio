31d8c6c9a6b4e62d49d93376ffcadb0da96d4a19
Jon Hagopian <jon.hagopian@gmail.com>
2017-10-08 18:31:32 -0400

    updated js

--- app/resources/js/headerImage.js
+++ app/resources/js/headerImage.js
@@ -3,9 +3,6 @@
   *batching reflows by browsers causes animation not to work on newly created elements, accessign offsetWidth triggers reflow and fixes this.
 */
 var imageBnrBox;
-var i = 1;
-var timeOut;
-var interval;
 
 function runHdrImg() {
   var imgElem = imageBnrBox.querySelectorAll("img");
@@ -13,41 +10,30 @@ function runHdrImg() {
       imgElem[i].style.opacity = 0;
     }
     imgElem[Math.floor(Math.random() * imgElem.length)].style.opacity = 1;
-    console.log("Running rotation complete");
 }//EOF
 
 function loadHdrImgs() {
-  if (i >= hdrImgArray.length) {
-    clearTimeout(timeOut);
-    interval = setInterval(runHdrImg, 10000);
-    console.log("cleared Timeout and runHdrImg");
-    return;
+  var i = 0;
+  function rotate() {
+    if (i >= hdrImgArray.length) {
+      clearTimeout(timeOut);
+      interval = setInterval(runHdrImg, 10000);
+      return;
+    }
+    newBannerImage = document.createElement("img");
+    newBannerImage.addEventListener("load", function() {
+      imageBnrBox.appendChild(newBannerImage);
+      var flowFix = newBannerImage.offsetWidth; // see note: *
+      newBannerImage.style.opacity = 1;
+    });
+    newBannerImage.src = defaultDomain + "/images/banner/" + hdrImgArray[i];
+    i++;
+    timeOut = setTimeout(rotate, 10000);
   }
-  newBannerImage = document.createElement("img");
-  newBannerImage.addEventListener("load", function() {
-    console.log("loaded header image: " + i);
-    imageBnrBox.appendChild(newBannerImage);
-    var flowFix = newBannerImage.offsetWidth; // see note: *
-    newBannerImage.style.opacity = 1;
-  });
-  newBannerImage.src = defaultDomain + "/images/banner/" + hdrImgArray[i];
-  i++;
-  timeOut = setTimeout(loadHdrImgs, 10000);
-}//EOF
-
-function startHdrImgs() {
-  newBannerImage = document.createElement("img");
-  newBannerImage.src = defaultDomain + "/images/banner/" + hdrImgArray[0]; 
-  newBannerImage.addEventListener("load", function() {
-    imageBnrBox.appendChild(newBannerImage);
-    var flowFix = newBannerImage.offsetWidth; // see note: *
-    newBannerImage.style.opacity = 1;
-    console.log("done start and loading hdr img func");
-    timeOut = setTimeout(loadHdrImgs, 1000);
-  });
+  rotate();
 }//EOF
 
 window.addEventListener("load", function() {
   imageBnrBox = document.getElementById("header-image");
-  startHdrImgs();
+  loadHdrImgs();
 }); 
