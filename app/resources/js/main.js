274921261cf25a4caf534b0ddc2d98cc3ec60fdb
Jon Hagopian <jon.hagopian@gmail.com>
2017-12-01 16:58:08 -0500

    added IE less than 11

--- app/resources/js/main.js
+++ app/resources/js/main.js
@@ -1,6 +1,6 @@
 function detectIE() {
   if (navigator.userAgent.indexOf("MSIE") >= 0) {
-    document.write("You are using an unsupported version of Internet Explorer. Please upgrade to Internet Explorer 11 or use a different web browser.");
+    window.location = "/badbrowser.html"
   }
   return false;
 }
